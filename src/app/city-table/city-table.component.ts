8e2dac7de6fb0814811018931ccb1e36d84d40b7
Calebe-Dve <calebe.araujo101@gmail.com>
2024-10-08 09:18:01 -0300

    criando componente tabela de cidades

---
+++ src/app/city-table/city-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-city-table',
+  templateUrl: './city-table.component.html',
+  styleUrl: './city-table.component.css'
+})
+export class CityTableComponent {
+
+}
